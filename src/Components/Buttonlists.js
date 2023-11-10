738cb5cded64ab0dd9ab2f15ba3aa11d17dcf755
chakribontha <bonthachakri1@gmail.com>
2023-11-10 23:10:37 +0530

    Initialize project using Create React App

---
+++ src/Components/Buttonlists.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Buttonlists = () => {
+  return (
+    <div>
+      Buttonlists
+    </div>
+  )
+}
+
+export default Buttonlists
---
+++ src/Components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Buttonlists from './Buttonlists'
+import VideoContainer from './VideoContainer'
+
+const MainContainer = () => {
+  return (
+    <div>
+          <Buttonlists />
+          <VideoContainer/>
+    </div>
+  )
+}
+
+export default MainContainer
